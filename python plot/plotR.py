153a1d0b329d644610f443a914e8b6b2f30b4bba
Konstantinos Perrakis <32614658+peroze@users.noreply.github.com>
2023-04-04 13:12:30 +0300

    Add files via upload

---
+++ python plot/plotR.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 22:00:43 2020
+
+@author: Dani
+"""
+import matplotlib.pyplot as plt
+
+infile = open('outData.txt', 'r')
+for line in infile:
+    line=line.replace("[","")
+    line=line.replace("]","")
+    line=line.replace(" ","")
+    line=line.split(",")
+    x1=float(line[0])
+    x2=float(line[1])
+    y1=float(line[2])
+    y2=float(line[3])
+    
+    rectangle = [(x1,y1),(x1,y2),(x2,y2),(x2,y1)]
+
+    plt.plot(*zip(*(rectangle+rectangle[:1])), marker='')
+    
+infile = open('points.txt', 'r')  
+for line in infile:
+    line=line.replace("","")
+    line=line.split(",")
+    p1=float(line[0])
+    p2=float(line[1])
+    print(p1)
+    plt.plot([p1], [p2], 'rx')
+plt.savefig('filename.png', dpi=300)
